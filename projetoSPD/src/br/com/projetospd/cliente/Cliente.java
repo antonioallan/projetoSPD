39cf197577f54c179f09a6ce8ad1f35001db540b
allan <antonioallan.santos@gmail.com>
2015-05-19 13:21:18 -0300

    Alterarndo o cliente

--- projetoSPD/src/br/com/projetospd/cliente/Cliente.java
+++ projetoSPD/src/br/com/projetospd/cliente/Cliente.java
@@ -10,19 +10,24 @@ public class Cliente {
 	
 	public static void main(String[] args) throws UnknownHostException, IOException {
 		
-		Socket cliente = new Socket("127.0.0.1",1234);
-		System.out.println("Cliente se conectou");
-		
-		Scanner teclado = new Scanner(System.in);
-		PrintStream saida = new PrintStream(cliente.getOutputStream());
-		
-		while(teclado.hasNextLine()){
-			saida.println(teclado.nextLine());
-		}
-		
-		saida.close();
-		teclado.close();
-		cliente.close();
+		Socket cliente = new Socket("127.0.0.1", 1234);
+        System.out.println("Cliente se conectou");
+
+        Scanner teclado = new Scanner(System.in);
+        PrintStream saida = new PrintStream(cliente.getOutputStream());
+        Scanner saidaServidor = new Scanner(cliente.getInputStream());
+
+        System.out.println("Informe a opereçao realizada(SOMA,SUBTRACAO,MULTIPLICACAO,DIVISAO)?");
+        saida.println(teclado.nextLine());
+        System.out.println("Informe o 1º numero");
+        saida.println(teclado.nextLine());
+        System.out.println("Informe o 2º numero");
+        saida.println(teclado.nextLine());
+        System.out.println(saidaServidor.nextLine());
+        saida.close();
+        teclado.close();
+        cliente.close();
+        saidaServidor.close();
 	}
 
 }
